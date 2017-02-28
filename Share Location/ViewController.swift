55dff72edc0b2d266b630a81d9b5861421e40af7
Dinesh Deore <deore3300@gmail.com>
2017-02-28 11:50:02 +0530

    Initial Commit

---
+++ Share Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Share Location
+//
+//  Created by Crystal-Logic on 28/02/17.
+//  Copyright © 2017 Crystal-Logic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
